f1cfb90c04c48ac3f3114450e1c8d8ccf6475918
Ahmed <ahmed.elshorafa2017@gmail.com>
2021-07-02 22:11:03 +0200

    Update upload_docker.sh

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+export dockerpath=ahmedadelelshorafa/house_prediction
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username ahmedadelelshorafa
+docker tag house_prediction $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
